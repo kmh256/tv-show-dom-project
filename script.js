839e66803c02d3b85b1c3db5baf8df9d44dcfd5e
Kara <kara.mh191@gmail.com>
2021-03-26 07:22:49 +0000

    Lesson 100 - Added cards
    Added bootstrap cards, episode headings, images and summaries.

--- script.js
+++ script.js
@@ -6,7 +6,36 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+  const cardDeck = document.createElement("div");
+  cardDeck.className = "card-deck justify-content-around";
+  
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.map((episode) => {
+    const episodeCard = document.createElement("div");
+    episodeCard.className = "col-sm-3 text-white bg-dark m-2";
+    const episodeImage = document.createElement("img");
+    episodeImage.className = "card-img-top mt-3";
+    episodeImage.src = episode.image.medium;
+    episodeCard.appendChild(episodeImage);
+    
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    const episodeHeader = document.createElement("h5");
+    episodeHeader.className = "card-title";
+    episodeHeader.innerHTML = `${episode.name}: S0${episode.season}E0${episode.number}`;
+    cardBody.appendChild(episodeHeader);
+
+    const episodeSummary = document.createElement("p");
+    episodeSummary.className = "card-text";
+    episodeSummary.innerHTML = episode.summary;
+    cardBody.appendChild(episodeSummary);
+
+    episodeCard.appendChild(cardBody);
+    cardDeck.appendChild(episodeCard);
+    rootElem.appendChild(cardDeck);
+
+  })
 }
 
 window.onload = setup;
